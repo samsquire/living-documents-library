d71295cdc8236fe660c7d5d02521824a18170a0a
Sam Squire <ssquire@thoughtworks.com>
2016-04-11 22:51:12 +0100

    stock data

--- stocks/main.js
+++ stocks/main.js
@@ -1,11 +1,18 @@
+var yahooFinance = require('node-yahoo-finance');
+
 function run (data, callback) {
   var purchase = data["stock purchase"];
+  var symbol = parseInt(purchase.questions[0].answer);
   var units = parseInt(purchase.questions[3].answer);
   var price = parseInt(purchase.questions[1].answer);
 
-  callback({
-    "profit/loss": units * price,
+  yahooFinance.snapshot({
+    symbol: symbol,
+    fields: ['s', 'n', 'd1', 'l1', 'y', 'r']
+  }, function (err, data) {
+    callback(data);
   });
+
 }
 
 module.exports = run;
